2fbf45d581431e1ded682670029d625b57d7c7a1
Esteban <acevedoesteban999@gmail.com>
2025-01-15 15:45:53 -0500

    UPD: No Ota Status UPD: Spiner and Reload Page

--- eOTA.c
+++ eOTA.c
@@ -1,18 +1,6 @@
 #include "eOTA.h"
 
 volatile bool OTA_BOOL = false;
-volatile enum eota_status OTA_STATUS = NO_OTA;
-volatile int status_value = 0;
-
-esp_err_t ota_status_post_handler(httpd_req_t *req)
-{
-    char buffer[25];
-    snprintf(buffer,sizeof(buffer),"status=%i&value=%i",OTA_STATUS,status_value);
-    httpd_resp_set_type(req, "application/x-www-form-urlencoded");
-    httpd_resp_send(req, buffer, strlen(buffer));
-    return ESP_OK;
-}
-
 
 void _ota_delayed_restart_task(void *pvParameter) {
     vTaskDelay(pdMS_TO_TICKS(1000));
@@ -23,36 +11,27 @@ void _ota_delayed_restart_task(void *pvParameter) {
 }
 
 esp_err_t ota_post_handler(httpd_req_t *req) {
-    ESP_LOGI("OTA", "OTA update started");
     OTA_BOOL = true;
-    OTA_STATUS = GET_PARTITION;
-    status_value = 0;
     esp_ota_handle_t ota_handle;
     const esp_partition_t *ota_partition = esp_ota_get_next_update_partition(NULL);
     if (ota_partition == NULL) {
-        ESP_LOGE("OTA", "Failed to find OTA partition");
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to find OTA partition");
         OTA_BOOL = false;
         return ESP_FAIL;
     }
 
-    OTA_STATUS = BEGIN;
     esp_err_t err = esp_ota_begin(ota_partition, OTA_SIZE_UNKNOWN, &ota_handle);
     if (err != ESP_OK) {
-        ESP_LOGE("OTA", "OTA begin failed %i", err);
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA begin failed");
         OTA_BOOL = false;
         return err;
     }
     
-    status_value = RECIV;
     char buf[1024];
     int remaining = req->content_len;
     while (remaining > 0) {
-        status_value =  100 * (req->content_len - remaining)/req->content_len;
         int recv_len = httpd_req_recv(req, buf, MIN(remaining, sizeof(buf)));
         if (recv_len <= 0) {
-            ESP_LOGE("OTA", "OTA receive failed");
             esp_ota_end(ota_handle);
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA receive failed");
             OTA_BOOL = false;
@@ -60,7 +39,6 @@ esp_err_t ota_post_handler(httpd_req_t *req) {
         }
         err = esp_ota_write(ota_handle, buf, recv_len);
         if (err != ESP_OK) {
-            ESP_LOGE("OTA", "OTA write failed %i", err);
             esp_ota_end(ota_handle);
             httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA write failed");
             OTA_BOOL = false;
@@ -69,26 +47,20 @@ esp_err_t ota_post_handler(httpd_req_t *req) {
         remaining -= recv_len;
     }
 
-    status_value = END;
     err = esp_ota_end(ota_handle);
     if (err != ESP_OK) {
-        ESP_LOGE("OTA", "OTA end failed %i", err);
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA end failed");
         OTA_BOOL = false;
         return err;
     }
 
-    status_value = SET_PARTITION;
     err = esp_ota_set_boot_partition(ota_partition);
     if (err != ESP_OK) {
-        ESP_LOGE("OTA", "OTA set boot partition failed %i", err);
         httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "OTA set boot partition failed");
         OTA_BOOL = false;
         return err;
     }
 
-    status_value = OK_RESET;
-    ESP_LOGI("OTA", "OTA update successful, rebooting...");
     httpd_resp_sendstr(req, "OTA update successful, rebooting...");
     xTaskCreate(&_ota_delayed_restart_task, "delayed_restart", 2048, NULL, 5, NULL);
     return ESP_OK;
--- src/js/ota.js
+++ src/js/ota.js
@@ -1,5 +1,7 @@
-const alertMessage = document.getElementById('alertMessage');
-var timeout;
+const messageContainer = document.getElementById('messageContainerID');
+const message = document.getElementById('messageID');
+const spiner = document.getElementById('spinerID');
+
 
 document.getElementById('otaForm').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -16,82 +18,37 @@ document.getElementById('otaForm').addEventListener('submit', (event) => {
         alert('Please select a .bin file!');
         return;
     }
-
-    const fileData = file.arrayBuffer();
-    
-    fetch('/ota_update', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/octet-stream',
-        },
-        body: fileData, 
-    }).then(response => {
-        clearTimeout(timeout);
-        if (response.ok) {
-            alertMessage.textContent = 'OTA Update successful! Rebooting ...';
-            alertMessage.className = 'alert success';
-            alertMessage.style.display = 'block';
-        } else {
-            response.text().then(errorMessage => {
-                console.error(`OTA Update failed: ${response.status} ${errorMessage}`)
-                alertMessage.textContent = `OTA Update failed: ${response.status} ${errorMessage}`;
-                alertMessage.className = 'alert error';
-                alertMessage.style.display = 'block';
-            });
-        }
-    }).catch(error => {
-        clearTimeout(timeout);
-        console.error('Error during OTA Update:', error);
-        alertMessage.textContent = 'Error during OTA Update. See console for details.';
-        alertMessage.className = 'alert error';
-        alertMessage.style.display = 'block';
+    messageContainer.className = 'alert loading';
+    message.textContent = `OTA Update in Progress ... `;
+    spiner.style.display = "block";
+    file.arrayBuffer().then(fileData => {
+        fetch('/ota_update', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/octet-stream',
+            },
+            body: fileData, 
+        }).then(response => {
+            response.text().then(data => {
+                if (response.ok) {
+                    messageContainer.className = 'alert success';
+                    message.textContent = data;
+                    spiner.style.display = "none";
+                    setTimeout(() => {
+                        location.reload(true);
+                    }, 1000);
+                } else {
+                    console.error(`OTA Update failed: ${response.status} ${data}`)
+                    messageContainer.className = 'alert error';
+                    message.textContent = `OTA Update failed: ${response.status} ${data}`;
+                    spiner.style.display = "none";
+                }
+            })
+        }).catch(error => {
+            console.error('Error during OTA Update:', error);
+            messageContainer.className = 'alert error';
+            message.textContent = 'Error during OTA Update. See console for details.';
+            spiner.style.display = "none";
+        });
     });
-
-    timeout = setTimeout(checkOtaStatus,1);
-
-
 });
-
-function checkOtaStatus() {
-    fetch('/ota_status', {
-        method: 'POST'
-    })
-    .then(response => {
-        response.text().then(data => {
-            if(response.ok){
-                const params = new URLSearchParams(data);
-                const status = params.get('status');  
-                const value = params.get('value');    
-                let statusMessage = '';
-
-                if (status === '0')
-                    statusMessage = 'Wait OTA progress';
-                else if (status === '1')
-                    statusMessage = 'Getting partition...';
-                else if (status === '2')
-                    statusMessage = 'OTA process started...';
-                else if (status === '3')
-                    statusMessage = `Receiving OTA data... ${value}% completed`;
-                else if (status === '4')
-                    statusMessage = 'OTA process ended, preparing boot partition...';
-                else if (status === '5')
-                    statusMessage = 'Setting boot partition...';
-                else if (status === '6')
-                    statusMessage = 'OTA update successful, rebooting...';
-                else
-                    statusMessage = 'Unknown status';
-                
-                console.log('OTA status:', status, 'value:', value);
-                alertMessage.textContent = statusMessage;
-                alertMessage.className = 'alert warning';
-                alertMessage.style.display = 'block';
-            }
-            else
-                console.error(`OTA Update Status failed: ${response.status} ${data}`)
-        })
-    }) 
-    .catch(error => {
-        console.error('Error fetching OTA status:', error);
-    });
-    timeout = setTimeout(checkOtaStatus,1000);
-}

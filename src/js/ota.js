89dc6e71eacba87917c49a67cb8451355b97a81f
Esteban <acevedoesteban999@gmail.com>
2025-01-17 17:10:48 -0500

    UPD: Add Ota Html Drop

--- src/js/ota.js
+++ src/js/ota.js
@@ -4,19 +4,50 @@ const spiner = document.getElementById("spinerID");
 const messageFile = document.getElementById("messageFileID");
 const buttonSubmit = document.getElementById("buttonSubmitID");
 const inputFile = document.getElementById("inputFileID");
+const dropZone = document.getElementById("dropZoneID");
 
-document.getElementById("inputFileID").addEventListener("change", (event) => {
-  const file = event.target.files[0];
+dropZone.addEventListener("click", () => {
+  inputFile.click();
+});
+
+dropZone.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  dropZone.style.borderColor = "#00bfff";
+});
+
+dropZone.addEventListener("dragleave", () => {
+  dropZone.style.borderColor = "#ccc";
+});
+
+dropZone.addEventListener("drop", (event) => {
+  event.preventDefault();
+  console.log(event.dataTransfer.files.length);
+  if (event.dataTransfer.files.length > 0) {
+    file = event.dataTransfer.files[0];
+    dataTransfer = new DataTransfer();
+    dataTransfer.items.add(file);
+    inputFile.files = dataTransfer.files;
+    selectFile(file);
+  }
+});
+
+inputFile.addEventListener("change", (event) => {
+  selectFile(event.target.files[0]);
+});
+
+function selectFile(file) {
   messageFile.innerHTML =
     "<span style='color: black !important'>File: </span>" + file.name;
   if (file.name.endsWith(".bin")) {
     messageFile.style.color = "green";
+    dropZone.style.borderColor = "green";
     buttonSubmit.disabled = false;
   } else {
     messageFile.style.color = "red";
+    dropZone.style.borderColor = "red";
     buttonSubmit.disabled = true;
   }
-});
+}
 
 document.getElementById("otaForm").addEventListener("submit", (event) => {
   event.preventDefault();
